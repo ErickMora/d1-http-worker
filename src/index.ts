ed5b62d9d72c3dba18a0e2d8325c4c9ef9220dd9
ErickMora <emora@lynxsource.com>
2025-12-04 13:59:25 -0500

    d1 worker project

---
+++ src/index.ts
@@ -0,0 +1,78 @@
+import { Hono } from "hono";
+import { bearerAuth } from "hono/bearer-auth";
+import { logger } from "hono/logger";
+import { prettyJSON } from "hono/pretty-json";
+
+type Bindings = {
+	DB: D1Database;
+  	API_KEY: string;
+};
+
+const app = new Hono<{ Bindings: Bindings }>();
+
+app.use("*", prettyJSON(), logger(), async (c, next) => {
+  const auth = bearerAuth({ token: c.env.API_KEY });
+  return auth(c, next);
+});
+
+app.post('/api/all', async (c) => {
+  try {
+    let { query, params } = await c.req.json();
+    let stmt = c.env.DB.prepare(query);
+    if (params) {
+      stmt = stmt.bind(params);
+    }
+
+    const result = await stmt.run();
+    return c.json(result);
+  } catch (err) {
+    return c.json({ error: `Failed to run query: ${err}` }, 500);
+  }
+});
+
+/**
+* Executes the `db.exec()` method.
+* https://developers.cloudflare.com/d1/worker-api/d1-database/#exec
+*/
+
+app.post('/api/exec', async (c) => {
+  try {
+    let { query } = await c.req.json();
+    let result = await c.env.DB.exec(query);
+    return c.json(result);
+  } catch (err) {
+    return c.json({ error: `Failed to run query: ${err}` }, 500);
+  }
+});
+
+/**
+* Executes the `db.batch()` method.
+* https://developers.cloudflare.com/d1/worker-api/d1-database/#batch
+*/
+
+app.post('/api/batch', async (c) => {
+  try {
+    let { batch } = await c.req.json();
+    let stmts = [];
+    for (let query of batch) {
+      let stmt = c.env.DB.prepare(query.query);
+      if (query.params) {
+        stmts.push(stmt.bind(query.params));
+      } else {
+        stmts.push(stmt);
+      }
+    }
+    const results = await c.env.DB.batch(stmts);
+    return c.json(results);
+  } catch (err) {
+    return c.json({ error: `Failed to run query: ${err}` }, 500);
+  }
+});
+
+export default app;
+/*
+export default {
+	async fetch(request, env, ctx): Promise<Response> {
+		return new Response('Hello World!');
+	},
+} satisfies ExportedHandler<Env>;*/
