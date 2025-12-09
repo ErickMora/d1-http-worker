6d399b2ecfa68f998f914aed06acecac5ee4afc3
ErickMora <emora@lynxsource.com>
2025-12-09 16:12:58 -0500

    fix object type to string

--- src/index.ts
+++ src/index.ts
@@ -20,21 +20,18 @@ app.post('/api/all', async (c) => {
     let { query, params } = await c.req.json();
     let stmt = c.env.DB.prepare(query);
     if (params) {
-      const cleanParams = params.map((p:any) => {
-		if (p === null || p === undefined) return null;
+	const cleanParams = params.map((p:any) => {
+		if (p === null) return null;
 
-		if (typeof p === 'number') return p; // good
-		if (typeof p === 'string') return p; // good
-		if (typeof p === 'boolean') return p ? 1 : 0;
+		if (!isNaN(p)) {
+		return Number(p); // convert numeric strings to numbers
+		}
 
-		// handle Number objects or boxed values
-		if (p.valueOf) return p.valueOf();
-
-		return p.toString();
+		return String(p); // leave everything else as string
 	});
 
-	stmt = stmt.bind(cleanParams);
-    }
+	stmt = stmt.bind(...cleanParams);
+	}
 
     const result = await stmt.run();
     return c.json(result);
